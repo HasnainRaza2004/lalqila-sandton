7c9532144705309cd2ad9e6841956e6313066995
HasnainRaza2004 <hasnainraza.dev26@gmail.com>
2025-11-23 01:39:45 +0500

    Query client added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="auto" />
-      <Slot />
-    </>
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaProvider>
+        <QueryClientProvider client={queryClient}>
+          <StatusBar style="auto" />
+          <Slot />
+        </QueryClientProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
